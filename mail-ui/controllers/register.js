530f3d0a5a4371ddc83f467df6a06509e7de02d8
Juan Briones <JuanMBriones@outlook.com>
2022-07-05 17:11:50 -0500

    Improved email validation🛠

--- mail-ui/controllers/register.js
+++ mail-ui/controllers/register.js
@@ -3,17 +3,15 @@ import axios from 'axios';
 const registerUser = (userMail, setShowAlertDuplicated) => {
   axios.post(`${process.env.NEXT_PUBLIC_API_URL}/register/`, userMail)
     .then(res => {
-      console.log(res);
-
-      if (setShowAlertDuplicated) {
+      if (res.status === 201 && setShowAlertDuplicated) {
         setShowAlertDuplicated(false);
       }
     })
     .catch(err => {
-      console.log(err);
-      if (setShowAlertDuplicated) {
+      if (err.response.status === 400 && setShowAlertDuplicated) {
         setShowAlertDuplicated(true);
       }
+
     }
   );
 
